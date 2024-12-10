8923f99c75886d886af81ca3ad43000f7d4ead08
chikuma0 <chikuma@dera.ai>
2024-12-10 08:44:01 +0000

    Deploying to gh-pages from @ chikuma0/msk-portfolio@5fe497e2610db227851c0bc7675f7390f3c7e73f 🚀

---
+++ _next/static/chunks/app/layout-67505c27bdfb5bef.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{8833:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,9461,23)),Promise.resolve().then(l.t.bind(l,9324,23))},9324:()=>{},9461:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_f9cd68"}}},e=>{var s=s=>e(e.s=s);e.O(0,[20,441,517,358],()=>s(8833)),_N_E=e.O()}]);
